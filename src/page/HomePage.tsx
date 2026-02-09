5f5696d830b507113eaaa3a69a5f0bdec601aaf5
katrina <jostsone.k@gmail.com>
2026-02-09 21:48:39 +0200

    Updated about page text

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,35 +1,55 @@
-import { alpha, Paper, Typography } from '@mui/material';
+import { alpha, Box, Paper, Typography } from '@mui/material';
 
 export default function HomePage() {
   return (
+    <Paper
+      elevation={1}
+      sx={{
+        width: "100%",
+        minHeight: "calc(100vh - 140px)",
+        padding: "24px",
+        boxSizing: "border-box",
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Laipni lūdzam literārā teksta analīzes rīkā!
+      </Typography>
       <Paper
-          elevation={1}
-          sx={{
-            width: "100%",
-            minHeight: "calc(100vh - 140px)",
-            padding: "24px",
-            boxSizing: "border-box",
-          }}
+        elevation={2}
+        sx={(theme) => ({
+          backgroundColor: alpha(theme.palette.secondary.main, 0.1),
+          padding: "16px",
+          textAlign: "justify",
+        })}
       >
-        <Typography variant="h4" gutterBottom>
-          Šeit kaut kāds apraksts??
+        <Typography variant="body1" gutterBottom>
+          <Box>
+            Šī vietne ir veidota, lai palīdzētu skolēniem un skolotājiem pārskatāmi analizēt literārus tekstus, attīstīt
+            teksta analīzes prasmes un atvieglot eseju plānošanu.
+          </Box>
+          <Box  sx={{ pt: "20px"}}>
+            Sadaļā “augšupielāde” iespējams augšupielādēt tekstu PDF formātā un iezīmēt tajā nozīmīgus fragmentus, frāzes
+            un citātus. Labajā pusē redzamas dažādas literārās analīzes kategorijas – burciņas. Tās palīdzēs lasīšanas
+            laikā viegli pamanīt un atcerēties būtiskākās vietas, kā arī iezīmētos fragmentus iedalīt kategorijās,
+            tādējādi analizējot tekstu.
+          </Box>
+          <Box  sx={{ pt: "20px"}}>
+            Sadaļā “teksta analīze” visi iezīmētie fragmenti ir apkopoti pa kategorijām. Šeit tos var sakārtot vēl
+            precīzāk, piemēram, sadalīt pēc atsevišķiem tēliem vai valodas līdzekļiem (metaforām, salīdzinājumiem, u. c.),
+            kā arī atzīmēt svarīgākos citātus kā favorītus.
+          </Box>
+          <Box  sx={{ pt: "20px"}}>
+            Sadaļā “piezīmes” iespējams pārskatīt atlasītos citātus, plānot esejas struktūru un pierakstīt papildu
+            piezīmes. Tas palīdz soli pa solim pārvērst lasīšanas laikā radušās idejas skaidrā un argumentētā tekstā.
+          </Box>
+          <Box  sx={{ pt: "20px"}}>
+            Šis rīks ir paredzēts gan mācību darbam klasē, gan patstāvīgai literārā teksta analīzei mājās.
+          </Box>
+          <Box  sx={{ pt: "20px"}}>
+            Veiksmi darbā!
+          </Box>
         </Typography>
-        <Paper
-          elevation={2}
-          sx={(theme) => ({
-            backgroundColor: alpha(theme.palette.secondary.main, 0.1),
-            padding: "16px",
-            textAlign: "justify",
-          })}
-          >
-          <Typography variant="body1" gutterBottom>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet ante ac est egestas,
-            ac consequat libero bibendum. Nunc consequat porta urna at tincidunt. Mauris quis erat dui.
-            Pellentesque eleifend cursus odio, et ornare lectus semper nec. Nullam varius rutrum nisl ac dapibus.
-            Integer eu consectetur arcu. Ut est justo, faucibus aliquam quam at, scelerisque interdum ipsum.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-          </Typography>
-        </Paper>
       </Paper>
+    </Paper>
   );
 }
